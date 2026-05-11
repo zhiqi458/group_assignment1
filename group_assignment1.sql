cf27244406aa6777864d50060d3679f70ddaa060
unknown <khooyuxuan1227@gmail.com>
2026-05-11 14:37:04 +0800

    mysql refresh

--- group_assignment1.sql
+++ group_assignment1.sql
@@ -19,3 +19,25 @@ CREATE TABLE IF NOT EXISTS orders(
     `status`ENUM('PENDING','COMPLETED') DEFAULT 'PENDING',
     `created_time` TIMESTAMP DEFAULT current_timestamp
 );
+
+CREATE TABLE IF NOT EXISTS order_details (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `order_id` INT,
+    `item_name` VARCHAR(255),
+    `price` DECIMAL(10, 2),
+    `quantity` INT,
+    `remark` TEXT,
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+  `username` VARCHAR(50),
+  `password` VARCHAR(255),
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*ALTER TABLE `orders` ADD COLUMN `payment_method` VARCHAR(50) DEFAULT NULL;*/
+
+ALTER TABLE `orders` MODIFY `status` ENUM('Pending', 'Completed', 'Paid', 'Cancelled') NOT NULL;
